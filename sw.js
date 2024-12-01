a83767ac888510f37ca2d9d2005529fa5aa6fe35
213lgr <3334992882@qq.com>
2024-12-01 17:14:11 +0800

    Site updated: 2024-12-01 17:14:10

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1733043588301";
+const VERSION = "1733044353116";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
